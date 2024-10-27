276c0b1f69124811b453da542a9d2e10797c115d
Bekter <besedov347@gmail.com>
2024-10-27 16:11:19 +0300

    add yaml file for deploy

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("Node js ", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Hi Baktar");
+    expect(response.text).toBe("Hi Baktar.");
   });
 });
